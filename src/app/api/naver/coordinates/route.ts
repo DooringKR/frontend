43dbe05ef1a014e4f2d629b8de7e665351f3a802
JooKangSan <you04045@naver.com>
2025-04-25 22:37:27 +0900

    feat: 예상 배송시간 추가

---
+++ src/app/api/naver/coordinates/route.ts
@@ -0,0 +1,25 @@
+// app/api/coordinates/route.ts
+export async function POST(req: Request) {
+  const { address } = await req.json();
+
+  const res = await fetch(
+    `https://maps.apigw.ntruss.com/map-geocode/v2/geocode?query=${encodeURIComponent(address)}`,
+    {
+      method: "GET",
+      headers: {
+        "X-NCP-APIGW-API-KEY-ID": process.env.NAVER_CLIENT_ID!,
+        "X-NCP-APIGW-API-KEY": process.env.NAVER_CLIENT_SECRET!,
+      },
+    },
+  );
+  console.log(process.env.NAVER_CLIENT_ID, process.env.NAVER_CLIENT_SECRET);
+  const data = await res.json();
+  console.log("서버 응답:", data); // ✅ 서버 로그
+
+  if (!data.addresses?.[0]) {
+    return new Response(JSON.stringify({ error: "주소를 찾을 수 없습니다." }), { status: 400 });
+  }
+
+  const { x, y } = data.addresses[0];
+  return Response.json({ lat: parseFloat(y), lon: parseFloat(x) });
+}
