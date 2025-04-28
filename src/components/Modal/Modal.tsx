e1cd3376f9c1675cc1357f9e751c0c4ea3c114a4
JooKangSan <you04045@naver.com>
2025-04-28 18:44:57 +0900

    fix: modal 수정

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -13,16 +13,13 @@ function Modal({ isOpen, onClose, children }: ModalProps) {
 
   return (
     <div className="fixed inset-0 z-50 flex items-end justify-center">
-      {/* dim (뒤쪽) */}
       <div
         className="absolute inset-0 bg-black bg-opacity-50"
         onClick={onClose}
       />
-
-      {/* 모달 박스 (앞쪽) */}
       <div
         className="relative w-full max-w-md rounded-t-2xl bg-white p-6 z-10"
-        onClick={(e) => e.stopPropagation()} // 모달 내부 클릭시 닫히지 않게
+        onClick={(e) => e.stopPropagation()}
       >
         {children}
       </div>
