039ee2b18a0af26d8a321c8936668cb56e9ab346
JooKangSan <you04045@naver.com>
2025-05-03 03:32:21 +0900

    feat: route.ts 파일 생성

---
+++ src/app/api/checkcash/cabinet/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server';
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+  const res = await fetch(`${baseUrl}/api/checkcash/cabinet`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(body),
+  });
+
+  const data = await res.json();
+
+  return NextResponse.json(data);
+}
