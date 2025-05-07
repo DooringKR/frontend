d55e95834d8ca7e294627580c175ec95e117abc4
JooKangSan <you04045@naver.com>
2025-05-07 22:57:07 +0900

    fix: build error 수정

--- src/app/(home)/cart/checkorder/page.tsx
+++ src/app/(home)/cart/checkorder/page.tsx
@@ -15,6 +15,7 @@ import DeliveryScheduleSelector from "./_components/DeliveryScheduleSelector";
 import RecipientPhoneNumber from "./_components/RecipientPhoneNumber";
 
 function CheckOrder() {
+  if (typeof window === "undefined") return null;
   const { currentItem } = useCurrentOrderStore();
   const router = useRouter();
   const searchParams = useSearchParams();
