9397ac98eee37860c5517228a215ee4aaccdde77
PSY <163077953+psy020529@users.noreply.github.com>
2025-05-31 01:11:31 +0900

    local build completed

--- back/dist/index.js
+++ back/dist/index.js
@@ -7,6 +7,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const orderHandler_1 = require("./orderHandler");
+const authHandler_1 = require("./authHandler");
 const app = (0, express_1.default)();
 const PORT = 3001;
 app.use((0, cors_1.default)({
@@ -15,6 +16,9 @@ app.use((0, cors_1.default)({
 }));
 app.use(express_1.default.json());
 app.post('/order', orderHandler_1.handleOrderRequest);
+app.post('/api/auth/signin', authHandler_1.handleSignIn);
+app.post('/api/auth/signup', authHandler_1.handleSignUp);
+app.get('/api/auth/user', authHandler_1.handleUserInfo);
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
--- back/dist/orderHandler.js
+++ back/dist/orderHandler.js
@@ -1,20 +1,11 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.handleOrderRequest = void 0;
 const prismaClient_1 = __importDefault(require("./prismaClient"));
-const handleOrderRequest = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+const handleOrderRequest = async (req, res) => {
     const order = req.body;
     // 1. 주문자 및 수령자 정보 검증
     if (!order.user || !order.recipientPhoneNumber || !order.address1 || !order.deliveryDate) {
@@ -36,7 +27,7 @@ const handleOrderRequest = (req, res) => __awaiter(void 0, void 0, void 0, funct
     }
     try {
         // 4. Prisma를 통한 주문 정보 저장
-        const savedOrder = yield prismaClient_1.default.order.create({
+        const savedOrder = await prismaClient_1.default.order.create({
             data: {
                 userId: order.user.id,
                 userType: order.user.userType,
@@ -64,5 +55,5 @@ const handleOrderRequest = (req, res) => __awaiter(void 0, void 0, void 0, funct
         console.error('DB 저장 오류:', error);
         return res.status(500).json({ success: false, message: '서버 내부 오류' });
     }
-});
+};
 exports.handleOrderRequest = handleOrderRequest;
