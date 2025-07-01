2b18b3736b8097a9512d77fa27f62e920cdd2b2a
eundin <escience1202@gmail.com>
2025-07-01 10:11:00 +0900

    Feat: mypage 레이아웃 구현
    - pageName.ts에 마이페이지 변수명 추가
    - TopNavigator에서 마이페이지 전용 UI 조건부 렌더링 수정

--- src/app/(home)/mypage/page.tsx
+++ src/app/(home)/mypage/page.tsx
@@ -1,3 +1,21 @@
-export default function MyPage() {
-  return <div>마이페이지</div>;
+import TopNavigator from "@/components/TopNavigator/TopNavigator";
+
+import Footer from "../_components/Footer";
+
+interface MyPageProps {
+  userPhone: number;
+}
+
+export default function MyPage({ userPhone }: MyPageProps) {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <TopNavigator title="마이페이지" page="mypage" />
+      <div className="flex flex-1 flex-col px-5 pt-5">
+        <h3 className="mb-[5px] text-sm font-400 text-gray-600">가입한 휴대폰 번호</h3>
+        {/* 나중에 || 는 삭제 필요 */}
+        <h3 className="text-[23px] font-700 text-gray-900">{userPhone || "010-1111-1111"}</h3>
+      </div>
+      <Footer />
+    </div>
+  );
 }
