81fa8ad71522365b80622227decbd204c22dd23a
eundin <escience1202@gmail.com>
2025-07-01 11:15:58 +0900

    Fix: netlify 빌드오류 해결
    - 마이페이지 서버컴포넌트, 클라이언트 컴포넌트 분리
    - 유저 핸드폰 번호 zustand로 받아오게 수정

--- src/app/(home)/mypage/page.tsx
+++ src/app/(home)/mypage/page.tsx
@@ -1,21 +1,13 @@
-import TopNavigator from "@/components/TopNavigator/TopNavigator";
+import { Suspense } from "react";
 
-import Footer from "../_components/Footer";
+import MyPageClient from "./MyPageClient";
 
-interface MyPageProps {
-  userPhone: number;
-}
-
-export default function MyPage({ userPhone }: MyPageProps) {
+function MyPage() {
   return (
-    <div className="flex min-h-screen flex-col">
-      <TopNavigator title="마이페이지" page="mypage" />
-      <div className="flex flex-1 flex-col px-5 pt-5">
-        <h3 className="mb-[5px] text-sm font-400 text-gray-600">가입한 휴대폰 번호</h3>
-        {/* 나중에 || 는 삭제 필요 */}
-        <h3 className="text-[23px] font-700 text-gray-900">{userPhone || "010-1111-1111"}</h3>
-      </div>
-      <Footer />
-    </div>
+    <Suspense fallback={null}>
+      <MyPageClient />
+    </Suspense>
   );
 }
+
+export default MyPage;
