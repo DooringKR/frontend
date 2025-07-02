ed678660de5e25f0098777ad557df47f5b2ffa54
eundin <escience1202@gmail.com>
2025-07-02 10:45:08 +0900

    Fix: checkorder변수 값 수정 order -> cart

--- src/constants/pageName.ts
+++ src/constants/pageName.ts
@@ -1,4 +1,4 @@
 export const CUSTOMER_SERVICE_PAGE = "/customer-service";
 export const HOME_PAGE = "/";
 export const MY_PAGE = "/mypage";
-export const CHECK_ORDER_PAGE = "/order/checkorder";
+export const CHECK_ORDER_PAGE = "/cart/checkorder";
