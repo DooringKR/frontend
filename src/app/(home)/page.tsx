985c755740662a1b8ac53224467fbd6826b89e42
eundin <escience1202@gmail.com>
2025-07-09 14:18:54 +0900

    Fix: 홈페이지 레이아웃 수정

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -20,14 +20,12 @@ async function Page() {
 
   return (
     <div className="flex min-h-screen flex-col">
-      <main className="flex flex-1 flex-col px-5">
-        <TopNavigator page="/" isCartEmpty={true} />
+      <TopNavigator page="/" isCartEmpty={true} />
+      <Banner />
 
-        <Banner />
-        <div className="mt-10 flex flex-col gap-7">
-          <AddressIndicator deliverySchedule="" />
-          <HomeProductContainer />
-        </div>
+      <main className="mt-10 flex flex-col gap-7">
+        <AddressIndicator deliverySchedule="" />
+        <HomeProductContainer />
       </main>
 
       <Footer />
