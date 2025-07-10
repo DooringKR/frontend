f55bb44607cba1d8bd5aac4f56683578a2ae6a78
eundin <escience1202@gmail.com>
2025-07-10 13:47:10 +0900

    Feat: 픽업 주소 버튼 클릭 시 모달 열리도록 구현
    - 픽업주소 모달 구현
    - 모달 내 주소복사 기능 추가

--- src/app/(home)/cart/receive-option/ReceiveOptionClient.tsx
+++ src/app/(home)/cart/receive-option/ReceiveOptionClient.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
+import BottomButton from "@/components/BottomButton/BottomButton";
+import Modal from "@/components/Modal/Modal";
 import TopNavigator from "@/components/TopNavigator/TopNavigator";
 
 import { useOrderStore } from "@/store/orderStore";
@@ -12,6 +15,8 @@ export default function ReceiveOptionClientPage() {
   const router = useRouter();
   const setReceiveMethod = useOrderStore(state => state.setReceiveMethod);
 
+  const [isPickupAddressModalOpen, setIsPickupAddressModalOpen] = useState(false);
+
   const handleSelect = (method: "delivery" | "pickup") => {
     setReceiveMethod(method);
     if (method === "delivery") {
@@ -19,6 +24,12 @@ export default function ReceiveOptionClientPage() {
     } else {
       router.push("/cart/pickup");
     }
+  };
+
+  const handleCopyAddress = () => {
+    navigator.clipboard.writeText("경기도 남양주시 오남읍 양지로139번길 11-14");
+    alert("주소가 복사되었습니다!");
+    setIsPickupAddressModalOpen(false);
   };
 
   return (
@@ -46,9 +57,36 @@ export default function ReceiveOptionClientPage() {
             onClick={() => handleSelect("pickup")}
           />
         </div>
-        <span className="rounded-[10px] bg-gray-100 px-3 py-[9px] font-500">
+        <button
+          className="rounded-[10px] bg-gray-100 px-3 py-[9px] font-500"
+          onClick={() => setIsPickupAddressModalOpen(true)}
+        >
           픽업주소 (도어링 공장)
-        </span>
+        </button>
+        {isPickupAddressModalOpen && (
+          <Modal
+            isOpen={isPickupAddressModalOpen}
+            onClose={() => setIsPickupAddressModalOpen(false)}
+          >
+            <div className="mt-2">
+              <div className="flex flex-col gap-1">
+                <h1 className="text-xl font-700 text-gray-900">픽업주소 (도어링 공장)</h1>
+                <h2 className="text-sm font-400 text-gray-400">
+                  경기도 남양주시 오남읍 양지로139번길 11-14
+                </h2>
+              </div>
+            </div>
+            <BottomButton
+              type={"2buttons"}
+              button1Text="닫기"
+              button2Text="주소 복사"
+              button1Type="GrayLarge"
+              className="p-0 pt-5"
+              onButton1Click={() => setIsPickupAddressModalOpen(false)}
+              onButton2Click={handleCopyAddress}
+            />
+          </Modal>
+        )}
       </div>
     </div>
   );
