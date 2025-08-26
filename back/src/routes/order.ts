7b093f68ae44cfd6a8ac8582777c4864ce58c7d5
PSY <163077953+psy020529@users.noreply.github.com>
2025-08-26 19:36:58 +0900

    orderid get api added

--- back/src/routes/order.ts
+++ back/src/routes/order.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { createOrder } from "../controllers/orderController";
 import { getOrdersByUser } from "../controllers/orderController";
+import { getOrderById } from "../controllers/orderController";
 
 const router = Router();
 
 // POST /order — 주문 생성
 router.post("/", createOrder);
 router.get("/orders", getOrdersByUser);
+router.get("/:order_id", getOrderById);
 
 export default router;
