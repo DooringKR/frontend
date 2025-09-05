e7b91b3089a351f4fa6f0ae7d0fa8dfbba39901d
PSY <163077953+psy020529@users.noreply.github.com>
2025-09-05 18:37:31 +0900

    url pattern deleted

--- back/src/components/svg/svgGenerators/genCabinet.ts
+++ back/src/components/svg/svgGenerators/genCabinet.ts
@@ -58,14 +58,14 @@ export function genCabinetSvg(colorProps: ColorProps = {}) {
         { d: "M260 864V384", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
         { d: "M860 336L464 240V720L860 816V336Z", fill: "#F3F4F6" },
         { d: "M464 720L260 864L656 960L860 816L464 720Z", fill: "#E3E3E3" },
-        { d: "M464 240L260 384L656 480L860 336L464 240Z", fill: "url(#pattern0_6_54)", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
+    { d: "M464 240L260 384L656 480L860 336L464 240Z", fill: "white", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
         { d: "M464 480L192 672L588 768L860 576L464 480Z", fill: "#F3F4F6" },
         { d: "M464 720L158 936L554 1032L860 816L464 720Z", fill: "#F3F4F6" },
-        { d: "M588 528L192 432V672L588 768V528Z", fill: "url(#pattern1_6_54)", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
-        { d: "M554 792L158 696V936L554 1032V792Z", fill: "url(#pattern2_6_54)", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
+        { d: "M588 528L192 432V672L588 768V528Z", fill: "#F3F4F6", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
+        { d: "M554 792L158 696V936L554 1032V792Z", fill: "#F3F4F6", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
         { d: "M554 952L758 808V888L554 1032V952Z", fill: "#D1D5DC", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
         { d: "M588 688L792 544V624L588 768V688Z", fill: "#D1D5DC", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
-        { d: "M656 480L860 336V816L656 960V480Z", fill: "url(#pattern3_6_54)", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
+        { d: "M656 480L860 336V816L656 960V480Z", fill: "#F3F4F6", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
       ];
       paths.forEach((props) => {
         const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
@@ -99,15 +99,15 @@ export function genCabinetSvg(colorProps: ColorProps = {}) {
         { d: "M860 336L464 240V720L860 816V336Z", fill: "#F3F4F6" },
         { d: "M464 720L158 936L554 1032L860 816L464 720Z", fill: "#F3F4F6" },
         { d: "M464 240V720L260 864V384L464 240Z", fill: "#F3F4F6" },
-        { d: "M464 240L260 384L656 480L860 336L464 240Z", fill: "url(#pattern0_10_1270)", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
+    { d: "M464 240L260 384L656 480L860 336L464 240Z", fill: "white", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
         { d: "M588 708L792 564V624L588 768V708Z", fill: "#D1D5DC", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
         { d: "M622 564L826 420V480L622 624V564Z", fill: "#D1D5DC", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
         { d: "M554 952L758 808V888L554 1032V952Z", fill: "#D1D5DC", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
-        { d: "M656 480L860 336V816L656 960V480Z", fill: "url(#pattern1_10_1270)", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
+    { d: "M656 480L860 336V816L656 960V480Z", fill: "white", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
         { d: "M260 864V384", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
-        { d: "M622 504L226 408V528L622 624V504Z", fill: "url(#pattern2_10_1270)", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
-        { d: "M588 648L192 552V672L588 768V648Z", fill: "url(#pattern3_10_1270)", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
-        { d: "M554 792L158 696V936L554 1032V792Z", fill: "url(#pattern4_10_1270)", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
+    { d: "M622 504L226 408V528L622 624V504Z", fill: "white", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
+    { d: "M588 648L192 552V672L588 768V648Z", fill: "white", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
+    { d: "M554 792L158 696V936L554 1032V792Z", fill: "white", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
       ];
       paths.forEach((props) => {
         const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
@@ -142,16 +142,16 @@ export function genCabinetSvg(colorProps: ColorProps = {}) {
           { d: "M260 864V384", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
           { d: "M860 336L464 240V720L860 816V336Z", fill: "#F3F4F6" },
           { d: "M464 720L260 864L656 960L860 816L464 720Z", fill: "#E3E3E3" },
-          { d: "M464 240L260 384L656 480L860 336L464 240Z", fill: "url(#pattern0_10_1278)", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
+            { d: "M464 240L260 384L656 480L860 336L464 240Z", fill: "white", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
           { d: "M464 480L192 672L588 768L860 576L464 480Z", fill: "#F3F4F6" },
           { d: "M464 720L158 936L554 1032L860 816L464 720Z", fill: "#F3F4F6" },
-          { d: "M537 804L141 708V948L537 1044V804Z", fill: "url(#pattern1_10_1278)", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
+            { d: "M537 804L141 708V948L537 1044V804Z", fill: "white", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
           { d: "M537 964L741 820V900L537 1044V964Z", fill: "#D1D5DC", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
           { d: "M571 700L775 556V636L571 780V700Z", fill: "#D1D5DC", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
           { d: "M622 594L826 450V500L622 644V594Z", fill: "#D1D5DC", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
-          { d: "M656 480L860 336V816L656 960V480Z", fill: "url(#pattern2_10_1278)", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
-          { d: "M622 524L226 428V548L622 644V524Z", fill: "url(#pattern3_10_1278)", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
-          { d: "M571 540L175 444V684L571 780V540Z", fill: "url(#pattern4_10_1278)", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
+            { d: "M656 480L860 336V816L656 960V480Z", fill: "white", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
+            { d: "M622 524L226 428V548L622 644V524Z", fill: "white", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
+            { d: "M571 540L175 444V684L571 780V540Z", fill: "white", stroke: "black", strokeWidth: "4", strokeLinejoin: "round" },
         ];
         paths.forEach((props) => {
           const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
