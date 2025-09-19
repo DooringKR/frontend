a013ac9442376464af8190e8236c2a2cddc599f0
PSY <163077953+psy020529@users.noreply.github.com>
2025-09-19 15:29:43 +0900

    추가 수정 필요(임시 수정 중)

--- src/app/(home)/hardware/page.tsx
+++ src/app/(home)/hardware/page.tsx
@@ -1,3 +1,80 @@
 // 홈에서 하드웨어 선택 시 오는 페이지
 // '하드웨어 종류를 선택해주세요'
 
+"use client";
+
+import { CATEGORY_LIST, HARDWARE_CATEGORY_LIST } from "@/constants/category";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Suspense } from "react";
+
+import Header from "@/components/Header/Header";
+import TopNavigator from "@/components/TopNavigator/TopNavigator";
+import { useSingleCartStore } from "@/store/singleCartStore";
+
+function HardwareCategoryPage() {
+  const router = useRouter();
+  const type = "hardware";
+  const setCart = useSingleCartStore(state => state.setCart);
+
+  const category = CATEGORY_LIST.find(item => item.slug === type);
+  let header;
+  if (category) header = category.name;
+
+  // 카테고리별 리스트 매핑
+  const categoryLists = { hardware: HARDWARE_CATEGORY_LIST};
+
+  const categories = categoryLists[type as keyof typeof categoryLists] || [];
+
+  return (
+    <div className="flex flex-col">
+      <TopNavigator />
+      <Header size="Large" title={`${header} 종류를 선택해주세요`} />
+      <div className="grid w-full grid-cols-2 gap-x-3 gap-y-[40px] px-5 pb-5 pt-10">
+        {categories.map((category, idx) => (
+          <div
+            key={category.slug}
+            className="flex flex-1 cursor-pointer flex-col items-center gap-2"
+            onClick={() => {
+              //type, catergory 추가 후 다음 페이지로 이동
+            //   console.log(category.slug);
+            //   console.log(type);
+              // 'accessory' 타입은 SingleCart의 type에 할당할 수 없으므로 예외 처리
+              setCart({
+                type,
+                category: category.slug,
+              });
+              router.push(`/hardware/${category.slug}`);
+            }}
+          >
+            <div className="relative aspect-square w-full">
+              <Image
+                src={category.image}
+                alt={category.type || ''}
+                fill
+                style={{
+                  objectFit: "contain",
+                  verticalAlign: category.slug === "drawer" ? "top" : "middle",
+                }}
+                className="w-full h-full object-cover rounded-[28px] border-[2px] border-[rgba(3,7,18,0.05)]"
+              />
+            </div>
+            <div className="text-center text-[17px]/[24px] font-500 text-gray-500">
+              {category.type || ''}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function OrderPage() {
+  return (
+    <Suspense fallback={<div>로딩 중...</div>}>
+      <HardwareCategoryPage />
+    </Suspense>
+  );
+}
+
+export default OrderPage;
