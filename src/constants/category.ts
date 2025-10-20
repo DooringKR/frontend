d016636bd7e480a9c5bbf240711a1eb1cbf43abc
PSY <163077953+psy020529@users.noreply.github.com>
2025-10-20 16:48:45 +0900

    Reorder cabinet category list to maintain consistency in item arrangement

--- src/constants/category.ts
+++ src/constants/category.ts
@@ -40,9 +40,9 @@ export const CABINET_CATEGORY_LIST: Category[] = [
   { type: DetailProductType.LOWERCABINET, image: "/img/cabinet-category/Lower.png", slug: "lower" },
   { type: DetailProductType.UPPERCABINET, image: "/img/cabinet-category/Upper.png", slug: "upper" },
   { type: DetailProductType.DRAWERCABINET, image: "/img/cabinet-category/Drawers.png", slug: "drawer" },
-  { type: DetailProductType.OPENCABINET, image: "/img/cabinet-category/Open.png", slug: "open" },
-  { type: DetailProductType.TALLCABINET, image: "/img/cabinet-category/Tall.png", slug: "tall" },
   { type: DetailProductType.FLAPCABINET, image: "/img/cabinet-category/Flap.png", slug: "flap" },
+  { type: DetailProductType.TALLCABINET, image: "/img/cabinet-category/Tall.png", slug: "tall" },
+  { type: DetailProductType.OPENCABINET, image: "/img/cabinet-category/Open.png", slug: "open" },
 ]
 
 export const FINISH_CATEGORY_LIST: Category[] = [
